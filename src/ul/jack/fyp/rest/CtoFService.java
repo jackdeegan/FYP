e2c248ab5b04dae3f36c505e6a86153a0b59d41f
jackdeegan <jack.deegan@gmail.com>
2017-10-02 11:19:15 +0100

    Updated rest example

--- src/ul/jack/fyp/rest/CtoFService.java
+++ src/ul/jack/fyp/rest/CtoFService.java
@@ -1,33 +1,66 @@
+//rest example
+
 package ul.jack.fyp.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+
+import com.google.gson.Gson;
+
+import ul.jack.fyp.model.UsersDao;
  
-@Path("/ctofservice")
+@Path("/test")
 public class CtoFService {
 	@GET
-	@Produces("application/xml")
+	@Produces("application/json")
 	public String convertCtoF() {
- 
-		Double fahrenheit;
-		Double celsius = 36.8;
-		fahrenheit = ((celsius * 9) / 5) + 32;
- 
-		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
-		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+		UsersDao u = new UsersDao();
+		u.setFname("jason");
+		u.setLname("lastname");
+		u.setStudentNum(123);
+		u.setEmail("jason@test.com");
+		
+		Gson g = new Gson();
+		return g.toJson(u);
+
+		
+		
+//		Double fahrenheit;
+//		Double celsius = 36.8;
+//		fahrenheit = ((celsius * 9) / 5) + 32;
+// 
+//		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+//		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
 	}
  
-	@Path("{c}")
-	@GET
-	@Produces("application/xml")
-	public String convertCtoFfromInput(@PathParam("c") Double c) {
-		Double fahrenheit;
-		Double celsius = c;
-		fahrenheit = ((celsius * 9) / 5) + 32;
- 
-		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
-		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
-	}
+//	@Path("{c}")
+//	@GET
+//	@Produces("application/xml")
+//	public String convertCtoFfromInput(@PathParam("c") Double c) {
+//		Double fahrenheit;
+//		Double celsius = c;
+//		fahrenheit = ((celsius * 9) / 5) + 32;
+// 
+//		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+//		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+//	}
+	
+//	@GET
+//	@Path("/jason")
+//	@Produces("application/json")
+//	public String jsonTest() {
+//		UsersDao u = new UsersDao();
+//		u.setFname("jason");
+//		u.setLname("lastname");
+//		u.setStudentNum(123);
+//		u.setEmail("jason@test.com");
+//		
+//		Gson g = new Gson();
+//		return g.toJson(u);
+//
+//	}
 }
+
+//right methods for everything validate user etc
